7f741f0584ba2996b2f133545fc65b8244ddfa89
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-02-21 02:53:41 +0500

    Adjust vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Confirm this is set
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
   }
 })
