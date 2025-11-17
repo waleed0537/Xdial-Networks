c190e15d8ecd306818d756128f703278c31a8d23
waleed0537 <waleed75651@gmail.com>
2025-11-17 23:52:37 +0500

    Changed FIkles

--- models/index.js
+++ models/index.js
@@ -1,20 +1,6 @@
-// index.js
 const sequelize = require('../database');
 const ClientData = require('./Client');
 const Integration = require('./integration');
-
-// ClientData -> Integration relationship
-ClientData.hasMany(Integration, {
-  foreignKey: 'clientsdata_id',
-  sourceKey: 'id',
-  as: 'integrations'
-});
-
-Integration.belongsTo(ClientData, {
-  foreignKey: 'clientsdata_id',
-  targetKey: 'id',
-  as: 'clientData'
-});
 
 module.exports = {
   sequelize,
