66da8f28eb984a46a0355660c07ac6d78f3c5a62
waleed0537 <waleed75651@gmail.com>
2025-11-18 23:07:46 +0500

    Reverted code

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const sequelize = require('../database');
-const ClientData = require('./Client.cjs');
-const Integration = require('./integration.cjs');
+const ClientData = require('./Client');
+const Integration = require('./integration');
 
 module.exports = {
   sequelize,
